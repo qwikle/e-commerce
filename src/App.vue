b78314b45006f8d8cb17c6dfeb8f4bef53ca9b8d
Qwikle <qwikle.ziko@gmail.com>
2023-01-22 12:14:24 +0100

    add span cart

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,17 @@ import RouterView from 'route'
       </ul>
     </div>
     <div class="flex gap-3">
-      <button class="w-8 h-8">
+      <button class="w-10 h-10 flex justify-center items-center relative">
+        <!-- span quantity cart -->
+        <span
+          class="w-4 h-4 absolute rounded-md top-1 -right-0 text-white bg-[#ff7d1a] text-xs"
+          >3</span
+        >
         <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
             fill-rule="nonzero"
-            class="fill-slate-500"
+            class="fill-slate-500 w-full h-full"
           />
         </svg>
       </button>
